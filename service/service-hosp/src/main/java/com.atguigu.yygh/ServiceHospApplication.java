32cb8778f500e57a2ac867dec6ddfd20d2503f45
weivang <uquantum@Hotmail.com>
2022-02-14 02:05:19 +0800

    swagger配置

--- service/service-hosp/src/main/java/com.atguigu.yygh/ServiceHospApplication.java
+++ service/service-hosp/src/main/java/com.atguigu.yygh/ServiceHospApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.atguigu")
 public class ServiceHospApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceHospApplication.class, args);
