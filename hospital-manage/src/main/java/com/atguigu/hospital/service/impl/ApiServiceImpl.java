27aadccd75194e7fd1a5720847a54af4f6769d6d
mbp14-ww <uquantum@hotmail.com>
2022-11-26 12:53:12 +0800

    fix the database connection; fix the md5 encrypt;

--- hospital-manage/src/main/java/com/atguigu/hospital/service/impl/ApiServiceImpl.java
+++ hospital-manage/src/main/java/com/atguigu/hospital/service/impl/ApiServiceImpl.java
@@ -9,6 +9,7 @@ import com.atguigu.hospital.model.Schedule;
 import com.atguigu.hospital.service.ApiService;
 import com.atguigu.hospital.util.BeanUtils;
 import com.atguigu.hospital.util.HttpRequestHelper;
+import com.atguigu.hospital.util.MD5;
 import com.atguigu.hospital.util.YyghException;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.codec.binary.Base64;
@@ -99,7 +100,8 @@ public class ApiServiceImpl implements ApiService {
         paramMap.put("bookingRule",bookingRule.toJSONString());
 
         paramMap.put("timestamp", HttpRequestHelper.getTimestamp());
-        paramMap.put("sign", HttpRequestHelper.getSign(paramMap, this.getSignKey()));
+//        paramMap.put("sign", HttpRequestHelper.getSign(paramMap, this.getSignKey()));
+        paramMap.put("sign", MD5.encrypt(this.getSignKey()));
 
         JSONObject respone = HttpRequestHelper.sendRequest(paramMap,this.getApiUrl()+"/api/hosp/saveHospital");
         System.out.println(respone.toJSONString());
--- service/service-hosp/src/main/java/com/atguigu/yygh/service/impl/HospitalSetServiceImpl.java
+++ service/service-hosp/src/main/java/com/atguigu/yygh/service/impl/HospitalSetServiceImpl.java
@@ -4,6 +4,7 @@ import com.atguigu.yygh.model.hosp.HospitalSet;
 import com.atguigu.yygh.service.HospitalSetService;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
