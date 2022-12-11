13f10e9802b4e27146918ee16a8e73137cd45ecb
mbp14-ww <uquantum@hotmail.com>
2022-12-11 18:09:21 +0800

    网关拦截判断

--- service-gateway/src/main/java/com/atguigu/yygh/gateway/config/CorsConfig.java
+++ service-gateway/src/main/java/com/atguigu/yygh/gateway/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.atguigu.yygh.gateway;
+package com.atguigu.yygh.gateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
