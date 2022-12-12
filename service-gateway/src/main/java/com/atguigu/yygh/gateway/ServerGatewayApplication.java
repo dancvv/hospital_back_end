b7e5b706e8fe8d3e64799a85228da6bc943901db
mbp14-ww <uquantum@hotmail.com>
2022-12-12 21:54:25 +0800

    gateway fix

--- service-gateway/src/main/java/com/atguigu/yygh/gateway/ServerGatewayApplication.java
+++ service-gateway/src/main/java/com/atguigu/yygh/gateway/ServerGatewayApplication.java
@@ -2,8 +2,9 @@ package com.atguigu.yygh.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class ServerGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServerGatewayApplication.class, args);
